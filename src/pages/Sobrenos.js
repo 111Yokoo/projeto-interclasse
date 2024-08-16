74ed8de573484c6a556d941cab7d65f5b241ea2b
111Yokoo <yokoomatheus@gmail.com>
2024-08-16 13:56:00 -0300

    Finalização sobre nos

--- src/pages/Sobrenos.js
+++ src/pages/Sobrenos.js
@@ -21,77 +21,72 @@ function Sobrenos() {
         <section class="titulo">
           <h1>DEV</h1>
         </section>
-
         <section class="alinhamento">
-          <div class="imgesquerda">
-            <div>
+          <article class="imgesquerda">
+            <aside>
               <img class="img" src={Pereira} />
               <h2>Pedro Pereira</h2>
-            </div>
+            </aside>
             <div class="texto">Com um olhar atento para [área de interesse ou hobby], [Seu Nome] é também conhecido(a) por [alguma característica pessoal ou realização significativa]. Seus colegas e amigos frequentemente o/la descrevem como [adjetivo positivo], destacando a [qualidade específica, como empatia, criatividade, ou liderança].</div>
-          </div>
-          <div class="imgdireita">
-            <div>
+          </article>
+          <article class="imgdireita">
+            <aside>
               <img class="img" src={henrique} />
               <h2>Henrique Ganeko</h2>
-            </div>
+            </aside>
             <div class="texto">Com um olhar atento para [área de interesse ou hobby], [Seu Nome] é também conhecido(a) por [alguma característica pessoal ou realização significativa]. Seus colegas e amigos frequentemente o/la descrevem como [adjetivo positivo], destacando a [qualidade específica, como empatia, criatividade, ou liderança].</div>
-          </div>
+          </article>
         </section>
-
         <section class="alinhamento">
-          <div class="imgesquerda">
-            <div>
+          <article class="imgesquerda">
+            <aside>
               <img class="img" src={japa} />
               <h2>Matheus Yokoo</h2>
-            </div>
+            </aside>
             <div class="texto">Com um olhar atento para [área de interesse ou hobby], [Seu Nome] é também conhecido(a) por [alguma característica pessoal ou realização significativa]. Seus colegas e amigos frequentemente o/la descrevem como [adjetivo positivo], destacando a [qualidade específica, como empatia, criatividade, ou liderança].</div>
-          </div>
-          <div class="imgdireita">
-            <div>
+          </article>
+          <article class="imgdireita">
+            <aside>
               <img class="img" src={caue} />
               <h2>Cauê Pacheco</h2>
-            </div>
+            </aside>
             <div class="texto">Com um olhar atento para [área de interesse ou hobby], [Seu Nome] é também conhecido(a) por [alguma característica pessoal ou realização significativa]. Seus colegas e amigos frequentemente o/la descrevem como [adjetivo positivo], destacando a [qualidade específica, como empatia, criatividade, ou liderança].</div>
-          </div>
+          </article>
         </section>
-
         <section class="titulo">
           <h1>Multmídia</h1>
         </section>
-
         <section class="alinhamento">
-          <div class="imgesquerda">
-            <div>
+          <article class="imgesquerda">
+            <aside>
               <img class="img" src={giovanna} />
               <h2>Giovanna Quinalia</h2>
-            </div>
+            </aside>
             <div class="texto">Com um olhar atento para [área de interesse ou hobby], [Seu Nome] é também conhecido(a) por [alguma característica pessoal ou realização significativa]. Seus colegas e amigos frequentemente o/la descrevem como [adjetivo positivo], destacando a [qualidade específica, como empatia, criatividade, ou liderança].</div>
-          </div>
-          <div class="imgdireita">
-            <div>
+          </article>
+          <article class="imgdireita">
+            <aside>
               <img class="img" src={lara} />
               <h2>Lara Vitória</h2>
-            </div>
+            </aside>
             <div class="texto">Com um olhar atento para [área de interesse ou hobby], [Seu Nome] é também conhecido(a) por [alguma característica pessoal ou realização significativa]. Seus colegas e amigos frequentemente o/la descrevem como [adjetivo positivo], destacando a [qualidade específica, como empatia, criatividade, ou liderança].</div>
-          </div>
+          </article>
         </section>
-
         <section class="alinhamento">
-          <div class="imgesquerda">
-            <div>
+          <article class="imgesquerda">
+            <aside>
               <img class="img" src={martuccio} />
               <h2>Pedro Martuccio</h2>
-            </div>
+            </aside>
             <div class="texto">Com um olhar atento para [área de interesse ou hobby], [Seu Nome] é também conhecido(a) por [alguma característica pessoal ou realização significativa]. Seus colegas e amigos frequentemente o/la descrevem como [adjetivo positivo], destacando a [qualidade específica, como empatia, criatividade, ou liderança].</div>
-          </div>
-          <div class="imgdireita">
-            <div>
+          </article>
+          <article class="imgdireita">
+            <aside>
               <img class="img" src={otavio} />
               <h2>Otávio Cardoso</h2>
-            </div>
+            </aside>
             <div class="texto">Com um olhar atento para [área de interesse ou hobby], [Seu Nome] é também conhecido(a) por [alguma característica pessoal ou realização significativa]. Seus colegas e amigos frequentemente o/la descrevem como [adjetivo positivo], destacando a [qualidade específica, como empatia, criatividade, ou liderança].</div>
-          </div>
+          </article>
         </section>
       </main>
       <Footer />
