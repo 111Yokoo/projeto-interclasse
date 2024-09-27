2b61275975f523345069520d31dc79379259dbff
111Yokoo <yokoomatheus@gmail.com>
2024-09-27 11:39:35 -0300

    atualização destaque

--- src/components/DestaquesSemana/Index.js
+++ src/components/DestaquesSemana/Index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./DestaquesSemana.css";
-import Masculino from "../../assets/userMasculino.jpeg";
+import Masculino from "../../assets/user.jpg";
 import Feminino from "../../assets/userFeminino.jpg";
 
 function DestaquesSemana() {
@@ -8,7 +8,7 @@ function DestaquesSemana() {
     <section class="destaquesSessao">
         <section>
             <img src={Masculino} alt="Destaque Masculino" class="imgDestaque"/>
-            <h2>Guilherme Suith 3ºB</h2>
+            <h2>Leonardo Simões 2ºB</h2>
         </section>
         <section>
             <img src={Feminino} alt="Destaque Feminino" class="imgDestaque"/>
